345c0d03019209d61fdfbae8d6999ad1d59731bd
Gustav Eek <gustaveek@student.gu.se>
2010-05-19 15:33:34 +0200

    Ingen egentlig framgång, bara uppdatering.

--- evaluatePopulation.m
+++ evaluatePopulation.m
@@ -1,9 +1,19 @@
 % evaluatePopulation.m Evaluates all individuals in the population
-% rFitt = evaluatePopulation (aPop, aParam)
+% fitness = evaluatePopulation (population, param)
 %    
-%   @param cell array aPop:
-%   @param struct aParam: parameters.
+%   @param cell array population:
+%   @param struct param: parameters.
 %
 %   This includes generate transition matricies and a payoff matrix. 
+
+function rFitt = evaluatePopulation (aPop, aParam)
+
+   % generate payofMatrix
+   payoffMat = genPayoffMat (aPop)
+
+
+   % massa andraa saker.
+
+return
 
 
---
+++ genTransMat.m
@@ -0,0 +1,20 @@
+% genTransMat.m Returns transision matrix given a state table.
+% matrix = genTransMat (table, param)
+%
+%    @param N x 2 float matrix: table
+%    @param main parameter struct: param
+%    @return N x N float matrix: matrix
+%
+% The transition matrix contains the transition probabilities between
+% state I and J in the state table. N is number of possible history 
+% states.
+
+function rMat = genTransMat (aTab, aParam)
+
+  % find dimension
+  dim = size 
+
+
+
+   
+  
