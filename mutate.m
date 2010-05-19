03bd665b08593f7d19819bf6e1ce88f75c7e689e
Lars-Henrik Björnsson <larshenr@o-7156-05.studat.chalmers.se>
2010-05-19 14:41:21 +0200

    Lars-H försöker lära sig git

--- mutate.m
+++ mutate.m
@@ -5,7 +5,7 @@
 %    @param vector of int, inserted: indexies into previous parameter marking inserted pffspring
 %    @param struct, param: main parameters
 %    @return cell array newPopulation:
-%
+%sork
 %    Performes all different mutations. 
 
 function rPop = mutate (aPop, aInserted, aParam)
