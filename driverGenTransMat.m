da2b317da20ef7f5cc169eb789ffe3c6139cfa5e
Gustav Eek <gustaveek@student.gu.se>
2010-05-23 16:51:42 +0200

    Funktion genTransMat.m har viss funktionalitet. Dock fler tester måste göras, och ännu blir det fel med brus.

--- driverGenTransMat.m
+++ driverGenTransMat.m
@@ -1,58 +1,98 @@
 % driverGenTranMat.m Driver script for genTransMat
 %
-% Use first both tit for tat. 
+% Use first both tit for tat, which is do the oposit
 %
-%     Table           Matrix
-%   pI      pII       dd dc cd cc
-%    0  dd   0     dd  1  0  0  0
-%    1  dc   0	   dc  0  0  1  0
-%    0  cd   1     cd  0  1  0  0                            	    
-%    1  cc   1     cc  0  0  0  1                            	    
-% 
+%     Table           Matrix from
+%   pI      pII         dd dc cd cc
+%    0  dd   0       dd  1  0  0  0
+%    1  dc   0	  to dc  0  0  1  0
+%    0  cd   1       cd  0  1  0  0                            	    
+%    1  cc   1       cc  0  0  0  1                            	    
+%  
+% Uses cell arrray for to store test table, expected matrix, and 
+% output matrix. 
+
+
+clear all;
 
 % test prob == 0
 param = struct( 'pError', 0.0);
+ param.pError = 0.03; %rand(1);
+
+
+
+
+numTests = 3;
+tables  = cell(numTests,1);
+expMats = cell(numTests,1);
+outMats = cell(numTests,1);
+isOk = ones(1, numTests);
+
 
 % 1) two tit for tat
-table1 = [0 0;
+tables{1} = [0 0;
 	  1 0;
 	  0 1;
 	  1 1];
-expMat1 = [1 0 0 0;
+expMats{1} = [1 0 0 0;
 	   0 0 1 0;
 	   0 1 0 0;
 	   0 0 0 1];
-
-outMat1 = genTransMat (table, param);
+outMats{1} = genTransMat (tables{1}, param);
 
 % 2) tow tit for tat, double chromosome
-table2 = [0 0
-	  1 0
-	  0 1
-	  1 1
+tables{2} = [tables{1}; tables{1}; tables{1}; tables{1}];
+expMats{2} = zeros(size(tables{2}));
+for z = 1:4, 
+  expMats{2}( 1, (4*(z-1) + 1)) = 1;
+  expMats{2}( 7, (4*(z-1) + 2)) = 1;
+  expMats{2}(10, (4*(z-1) + 3)) = 1;
+  expMats{2}(16, (4*(z-1) + 4)) = 1;
+end
+outMats{2} = genTransMat (tables{2}, param);
 
-	  0 0
-	  1 0
-	  0 1 
-	  1 1
-
-	  0 0
-	  1 0
-	  0 1
-	  1 1
-
-	  0 0
-	  1 0
-	  0 1
-	  1 1];
+% 3) Random table size 16. Do not test output matrix agains expected matrix
+tables{3} = rand (64, 2);
+outMats{3} = genTransMat(tables{3}, param);
 
 
+% test: compare to expeced version
+for i = 1:2
+  if find (outMats{i} - expMats{i}) % then not OK
+    isOk(i) = 0;
+    disp (['Test ' num2str(i) ', output, not OK.']);
+    disp ('Table:');
+    disp (tables{i});
+    disp ('Expected:');
+    disp (expMats{i});
+    disp ('got');
+    disp (outMats{i});
+  else
+    isOk(i) = 1;
+  end
+end
 
-if (outMat == expMat)
-  disp('Test OK.');
+% test: check sum
+prec = 2.22e-15;
+for i = 1:numTests
+  sums = sum (outMats{i}, 1);
+  if find (abs(sums - 1.0) > prec)
+    isOk(i) = 0;
+    disp (['Test ' num2str(i) ', sum, not OK.']);
+    disp ('Sums:');
+    disp (sums);
+    disp ('Table:');
+    disp (tables{i});
+    disp ('Output marix:');
+    disp (outMats{i});
+  else
+    isOk(i) = 1;
+  end
+end
+
+if find( isOk ~= 1 )
+  disp ('Test not OK.');
+  disp (isOk);
 else
-  disp('Test not OK. Expected:');
-  disp(expMat);
-  disp('Result');
-  disp(outMat);
+  disp('Test OK.');
 end
--- genTransMat.m
+++ genTransMat.m
@@ -6,75 +6,76 @@
 %    @return N x N float matrix: matrix
 %
 % The transition matrix contains the transition probabilities between
-% state I and J in the state table. N is number of possible history 
+% state J and I in the state table. N is number of possible history 
 % states.
+%
+% This function can not yet handle noise and will exit with an error 
+% message.
 
 function rMat = genTransMat (aTab, aParam)
 
   % find dimension and simmilar operations
   dim = size (aTab, 1);
+  blockLevs = log2 (dim) / 2;
+  if (blockLevs ~= floor (blockLevs))
+    error ('Block level error.')
+  end
   prob = aParam.pError;
   prob4 = prob * prob;
   prob3 = (1 - prob) * prob;
   prob1 = (1 - prob) * (1 - prob);
 
-  rMat = zeros (dim);
-
-  if (dim == 4)
-    for i = 1:dim
-      for j = 1:dim
-	if (j == 1 || j == 2)
-	  term1 = 1 - aTab(i, 1);
-	else
-	  term1 = aTab(i, 1);
-	end
-	if (j == 1 || j == 3)
-	  term2 = 1 - aTab(i, 2);
-	else
-	  term2 = aTab(i, 2);
-	end
-	rMat(i, j) = ...
-	    term1 * term2 * prob1 + ...
-	    term1 * (1 - term2) * prob3 + ...
-	    (1 - term1) * term2 * prob3 + ...
-	    (1 - term1) * (1 - term2) * prob4;
-      end % loop over j
-    end % loop over i
-
-  elseif (dim == 16)
-    for i = 1:dim
-      for j = 1:dim
-
-	% calculate helping measures
-        x = 1 + rem (j - 1, 4); % row within block of four
-	y = 1 + rem (i - 1, 4); % column within block of four
-	z = floor ( (j - 1) / 4 ); % block number, row wise
-
-	if (z == y)
-	  if (x == 1 || x == 2)
-	    term1 = 1 - aTab(i, 1);
-	  else
-	    term1 = aTab(i, 1);
-	  end
-	  if (x == 1 || x == 3)
-	    term2 = 1 - aTab(i, 2);
-	  else
-	    term2 = aTab(i, 2);
-	  end
-	  rMat(i, j) = ...
-	      term1 * term2 * prob1 + ...
-	      term1 * (1 - term2) * prob3 + ...
-	      (1 - term1) * term2 * prob3 + ...
-	      (1 - term1) * (1 - term2) * prob4;
-	else
-	  rMat(i, j) = 0;
-	end
-      end % loop over j
-    end % loop over i
-
-  else % DEBUG
-    error ('genTransMat 1', 'Table dimension error.');
+  % FIX so that the function can coope with errors. 
+  if (prob ~= 0.0)
+    error('Noise level not zero.');
   end
+  
+  % initilize empty matrix
+  rMat = zeros (dim);
+  xMeas = zeros(1, blockLevs);
+  yMeas = zeros(1, blockLevs);
+
+  % start operate
+  for iTo = 1:dim
+
+    % generate measure x
+    for s = 1:blockLevs
+      xMeas(s) = 1 + rem (floor ((iTo - 1)/4^(s-1)), 4);
+    end
+
+    % for all jFrom
+    for yS = [1:4]
+
+      % get measure y
+      for s = 1:blockLevs - 1
+	yMeas(s) = xMeas(s + 1);
+      end
+      yMeas(blockLevs) = yS;
+
+      % get jFrom
+      jFrom =  1;
+      for s = 1:blockLevs
+	jFrom = jFrom + (yMeas(s) - 1) * 4^(s-1);
+      end
+      
+      % generate element in rMat
+      factorA = aTab(jFrom, 1);
+      if (xMeas(1) == 1 || xMeas(1) == 2)
+	factorA = 1 - aTab(jFrom, 1);
+      end
+      factorB = aTab(jFrom, 2);	
+      if (xMeas(1) == 1 || xMeas(1) == 3)
+	factorB = 1 - aTab(jFrom, 2);
+      end
+
+      rMat(iTo, jFrom) = ...
+	  factorA * factorB * prob1 + ...
+	  factorA * (1 - factorB) * prob3 + ...
+	  (1 - factorA) * factorB * prob3 + ...
+	  (1 - factorB) * (1 - factorB) * prob4;
+
+    end % for all jForm
+  end % for all iTo
   
 return 
 
