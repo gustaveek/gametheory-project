0b428a66122dc4674a57cf91cdf22220539c8397
Lars-Henrik Björnsson <larshenr@o-7156-06.studat.chalmers.se>
2010-05-26 11:22:48 +0200

    fungerande mutate.m med underfunktioner, en extra parameter lagd till main

--- mutateFloat.m
+++ mutateFloat.m
@@ -6,7 +6,15 @@ function  rFloatPopulation = mutateFloat(aPopulation, aParam)
 for i=1:size(aPopulation,1)
     for j=1:size(aPopulation{i},1)
         if rand<aParam.pMutFloat;
-            aPopulation{i}(j)=(aPopulation{i}(j)+sign(rand-0.5)*aParam.mutFloat);
+            aPopulation{i}(j)=(aPopulation{i}(j)+sign(rand-0.5)*aParam.mutFloatLength);
+            
+            if aPopulation{i}(j)<0
+                aPopulation{i}(j)=0;
+            end
+            if aPopulation{i}(j)>1
+                aPopulation{i}(j)=1;
+            end
+                
         end
     end
 end
