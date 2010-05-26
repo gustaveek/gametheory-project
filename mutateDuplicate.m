c670ce0d34cdbf63c72a2d51bc80fd8d20d29763
Lars-Henrik Björnsson <larshenr@o-7156-06.studat.chalmers.se>
2010-05-26 11:47:41 +0200

    mutateDuplicate spärrad så att den inte duplicerar kromosomer med längd 16

--- mutateDuplicate.m
+++ mutateDuplicate.m
@@ -1,5 +1,12 @@
 % mutateDuplicate.m duplicates a players genome and returns it 
 
 function  rDuplicatedGenome = mutateDuplicate(aGenome)
+
+
 rDuplicatedGenome=[aGenome;aGenome];
+
+if size(aGenome,1)==16
+rDuplicatedGenome=aGenome;
+end
+
 end
